41238c7b897dccf902804374da9842e76f96c45b
Jidai <lopesdacruzjeray@gmail.com>
2026-05-04 22:15:25 +0800

    Update AppServiceProvider.php

--- laravel-server/app/Providers/AppServiceProvider.php
+++ laravel-server/app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,7 +22,7 @@ class AppServiceProvider extends ServiceProvider
     {
         // \Illuminate\Support\Facades\Schema::defaultStringLength(191);
         if ($this->app->environment('production')) {
-            \Illuminate\Support\Facades\URL::forceScheme('https');
+            URL::forceScheme('https');
         }
     }
 }
