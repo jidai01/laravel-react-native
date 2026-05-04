0d8d664f5d68abc344d4b31e23f08d89db519131
Jidai <lopesdacruzjeray@gmail.com>
2026-05-04 21:01:57 +0800

    Update AppServiceProvider.php

--- laravel-server/app/Providers/AppServiceProvider.php
+++ laravel-server/app/Providers/AppServiceProvider.php
@@ -19,6 +19,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        \Illuminate\Support\Facades\Schema::defaultStringLength(191);
+        // \Illuminate\Support\Facades\Schema::defaultStringLength(191);
+        if ($this->app->environment('production')) {
+            \Illuminate\Support\Facades\URL::forceScheme('https');
+        }
     }
 }
