8cdfbe8206d9336f5f5e27aaaccaa8497a640732
Jidai <lopesdacruzjeray@gmail.com>
2026-05-04 22:09:15 +0800

    Update index.php

--- laravel-server/api/index.php
+++ laravel-server/api/index.php
@@ -1,12 +1,12 @@
 <?php
+// Pastikan autoload dan app dipanggil dengan benar
 require __DIR__ . '/../vendor/autoload.php';
 $app = require_once __DIR__ . '/../bootstrap/app.php';
 
+// Menangani request
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-
 $response = $kernel->handle(
     $request = Illuminate\Http\Request::capture()
 );
-
 $response->send();
 $kernel->terminate($request, $response);
